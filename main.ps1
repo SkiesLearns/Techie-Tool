fff25f36eba530238fe42f0bcc26eb35a8f35e64
SkiesLearns <max@skieslearns.com>
2024-11-20 15:26:44 +0000

    Initial Commit
    Initial commit, let's get crackin'

---
+++ main.ps1
@@ -0,0 +1,6 @@
+<#
+The purpose of this tool is:
+- Provide a all-in-one tool which can provide common functions and specific functions for System Administrators, Server Admins and for individuals.
+#>
+
+Write-Output "Hello World!" #TEst
