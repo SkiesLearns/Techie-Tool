f9bd1e08c5cf06cd37ddbf3c4c2235d58a34f18b
SkiesLearns <max@skieslearns.com>
2024-11-20 16:27:19 +0000

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -2,5 +2,13 @@
 The purpose of this tool is:
 - Provide a all-in-one tool which can provide common functions and specific functions for System Administrators, Server Admins and for individuals.
 #>
+$Current_Version = "1.0"
 
-Write-Output "Hello World!" #TEst
+Add-Type -AssemblyName System.Windows.Forms
+
+$TechieTool_Container = New-Object System.Windows.Forms.Form
+$TechieTool_Container.Text = "Techie Tool " + $Current_Version
+$TechieTool_Container.BackColor = "#e8e8e8"
+$TechieTool_Container.ClientSize = "1080,520"
+
+[void]$TechieTool_Container.ShowDialog()
